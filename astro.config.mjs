6162bfe6cd31203a698c9f1b64dc22f9e1b1be45
Gotheru <gotheru@gmail.com>
2025-09-01 08:47:18 +0200

    Change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://YOUR-USERNAME.github.io/REPO-NAME/',
+    base: '/REPO-NAME/',
+    outDir: 'dist',
+});
